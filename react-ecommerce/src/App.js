c23059e245202432cc15d56145cd69f56411fd07
dev1980 <brham1980@gmail.com>
2020-08-15 18:40:39 +0545

    create banner

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div>
      <Navbar />
+     <Banner />
     </div>
   );
 }
---
+++ react-ecommerce/src/components/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className="container">
+    <header>
+      <div className="headerText">
+      <div>
+        <h1>Festive Offer</h1>
+        <p>Enjoy Shopping for your family</p>
+      </div>
+      </div>
+    </header>
+    </div>
+
+  )
+}
+
+export default Banner;
